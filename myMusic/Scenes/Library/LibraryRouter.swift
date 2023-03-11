bc84fc37803e40b81b25df22ff093e17f6333601
EvgeniyLevchenko <evgeniy.levchnko@gmail.com>
2023-03-11 16:40:13 +0200

    library screen setup

---
+++ myMusic/Scenes/Library/LibraryRouter.swift
@@ -0,0 +1,21 @@
+//
+//  LibraryRouter.swift
+//  myMusic
+//
+//  Created by QwertY on 11.03.2023.
+//  Copyright (c) 2023 ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+import UIKit
+
+protocol LibraryRoutingLogic {
+    
+}
+
+class LibraryRouter: NSObject, LibraryRoutingLogic {
+    
+    weak var viewController: LibraryViewController?
+    
+    // MARK: Routing
+    
+}
